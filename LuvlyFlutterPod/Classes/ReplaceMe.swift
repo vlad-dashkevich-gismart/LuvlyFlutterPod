16c16271c0c60b9b512c0d86d5a703f87706f1c0
Vladislav Dashkevich <>
2025-02-11 15:17:48 +0100

    change

--- LuvlyFlutterPod/Classes/ReplaceMe.swift
+++ LuvlyFlutterPod/Classes/ReplaceMe.swift
@@ -3,18 +3,24 @@ import Flutter
 import FlutterPluginRegistrant
 
 public class RunEngine {
-    lazy var flutterEngine = FlutterEngine(name: "my flutter engine")
+//    lazy var flutterEngine = FlutterEngine(name: "my flutter engine")
     
     public init() {
-        flutterEngine.run()
-        GeneratedPluginRegistrant.register(with: self.flutterEngine);
+//        flutterEngine.run()
+//        GeneratedPluginRegistrant.register(with: self.flutterEngine);
     }
     
     public func getInitialController() -> UIViewController {
-        FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        let flutterEngine = FlutterEngine(name: "my_engine_2")
+        flutterEngine.run()
+        let viewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        
+        return viewController
     }
     
     public func getController(by route: String) -> UIViewController {
+        let flutterEngine = FlutterEngine(name: "my_engine")
+        flutterEngine.run(withEntrypoint: nil, initialRoute: "/greeting")
         let viewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
         viewController.setInitialRoute(route)
         
